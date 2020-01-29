4eda07ca7c6322f203d33d4ea837f41daac68344
eko <ekomuliyo.com>
2020-01-29 17:40:28 +0700

    clear folder

--- laravel/blade-templating/resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout')
-
-
-@section('content')
-
-    <h1>this is Home laravel 5.8</h1>
-
-@endsection
--- laravel/views/resources/views/contact.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>Contact Us</h1>
-
-<p>PT. Succecsfully</p>
-<h2>0907097097</h2>
